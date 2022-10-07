a9658bc383c1110a518605b5f860446615ef202d
fawad irfan sheikh <fawad313@yahoo.com>
2022-10-07 22:12:58 +0400

    change db name to loyalty.db

--- db_setup.py
+++ db_setup.py
@@ -13,7 +13,7 @@ import os
 app = Flask(__name__)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(base_dir, 'loyality.db')
+    os.path.join(base_dir, 'loyalty.db')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
